32e7c85385045ddf0f1dcff8070277c1bec774e8
Harsha K N <harshkn@gmail.com>
2017-08-03 17:06:01 +0530

    main functionality implemented

--- image_warp.py
+++ image_warp.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import numpy as np
 import cv2
 import glob
@@ -29,6 +30,21 @@ class ImageWarper:
 
     def get_normal_view(self, img):
         return cv2.warpPerspective(img, self.M_inv, dsize=(img.shape[1], img.shape[0]), flags=cv2.INTER_LINEAR)
+
+    def get_normal_view_points(self, x_val, y_val):
+        temp = len(x_val) * [1]
+
+        ret_list = []
+        new_points = [(p, y_val[ind], temp[ind]) for ind, p in enumerate(x_val)]
+        # print(new_points)
+        for p in new_points:
+            p = np.array(p)
+            new_point = np.dot(self.M_inv, p)
+            new_point = new_point / new_point[2]
+            ret_list.append((int(new_point[0]), int(new_point[1])))
+        return ret_list
+
+
 
 if __name__ == "__main__":
     cp = os.getcwd()
